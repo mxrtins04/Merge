c19179cfdc2a270a02394800030239467012ad91
Adewole Adedotun <erik.edwards.jon@gmail.com>
2026-07-01 13:55:59 -0700

    Extend ScoutService interface with Layer 3 methods: getLayer3Task, submitLayer3

--- backend/src/main/java/com/merge/backend/scout/service/ScoutService.java
+++ backend/src/main/java/com/merge/backend/scout/service/ScoutService.java
@@ -1,15 +1,12 @@
 package com.merge.backend.scout.service;
 
-import com.merge.backend.scout.dto.Layer1QuestionsResponse;
-import com.merge.backend.scout.dto.Layer1SubmitRequest;
-import com.merge.backend.scout.dto.Layer1SubmitResponse;
-import com.merge.backend.scout.dto.Layer2ProblemsResponse;
-import com.merge.backend.scout.dto.Layer2SubmitRequest;
-import com.merge.backend.scout.dto.Layer2SubmitResponse;
+import com.merge.backend.scout.dto.*;
 
 public interface ScoutService {
     Layer1QuestionsResponse getLayer1Questions();
     Layer1SubmitResponse submitLayer1(String studentEmail, Layer1SubmitRequest request);
     Layer2ProblemsResponse getLayer2Problems();
     Layer2SubmitResponse submitLayer2(String studentEmail, Layer2SubmitRequest request);
+    Layer3TaskResponse getLayer3Task(String studentEmail);
+    Layer3SubmitResponse submitLayer3(String studentEmail, Layer3SubmitRequest request);
 }
